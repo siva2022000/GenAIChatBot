10a23f4a4ff09346075c78f9c9e1b3878b1141f5
siva2022000 <sivamohan.mukkara@gmail.com>
2024-12-18 00:42:05 +0530

    removed ml server startup reload

--- MachineLearning/startServer.py
+++ MachineLearning/startServer.py
@@ -6,4 +6,4 @@ load_dotenv()
 port = int(os.getenv("PORT", 8000))
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
